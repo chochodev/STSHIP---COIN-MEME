cbb597335958ce4eb5ab137d56ea5c29ce22bd6a
Emmanuel Michael <michochoprogrammer@gmail.com>
2023-12-31 16:43:29 +0100

    setting the hierarchy

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Hero from './s1_hero';
 import HowToBuy from './s2_howtobuy';
+import Charts from './s3_trackingcharts';
+import Tokenomic from './s4_tokenomics';
+import Update from './s5_updates';
 
 const Home = () => {
   return (
     <>
     <Hero />
     <HowToBuy />
+    <Charts />
+    <Tokenomic />
+    <Update />
     </>
   )
 }
