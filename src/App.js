d0b18c3f266c5922d52931b3b7bef3a4d24fbec7
Emmanuel Michael <michochoprogrammer@gmail.com>
2023-12-31 16:23:24 +0100

    setting the hierarchy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from 'views/home';
 
 function App() {
   return (
     <div>
-      
+      <Home />
     </div>
   );
 }
