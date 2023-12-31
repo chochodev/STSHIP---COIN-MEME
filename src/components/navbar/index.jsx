969ea278b5508b5bc66260a44c778f515d242975
Emmanuel Michael <michochoprogrammer@gmail.com>
2023-12-31 17:16:35 +0100

    setting the hierarchy

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <div className="flex">
+        <div className="">
+          <h2 className='font-montserrat'>$tarship</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
