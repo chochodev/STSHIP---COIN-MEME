c12410411890cefbd60cb9b4e824c83bda023ca0
Emmanuel Michael <michochoprogrammer@gmail.com>
2023-12-31 21:10:43 +0100

    updating the navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     <div>
       <div className='flex'>
         <div className='flex gap-[.25rem]  '>
-          <h2 className='text-xl font-montserrat font-bold  '>$tarship</h2>
-          <img className='h-[4.06rem] w-[4.06rem] object-fit ' src='assets/images/logo-lg.png' alt='LOGO' />
+          <h2 className='text-[2.1875rem] font-montserrat font-[800]  '>$tarship</h2>
+          <img className='h-[4.0625rem] w-[4.0625rem] object-fit ' src='assets/images/logo-lg.png' alt='LOGO' />
         </div>
       </div>
     </div>
