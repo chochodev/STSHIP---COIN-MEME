0c6b5580f2c2d5fe79b83461111bb668e2fe876f
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-01-02 16:26:47 +0100

    made the q&a responsive

--- src/views/home/s9_q&a/index.jsx
+++ src/views/home/s9_q&a/index.jsx
@@ -6,11 +6,11 @@ const QACard = ({question, answer}) => {
   return (
     <div className='w-full'>
       <button className={`${isOpen? 'rounded-tr-[8px] rounded-tl-[8px]' : 'rounded-[8px]'} flex justify-between items-center text-center w-full p-[1rem] bg-secondary-dark border-solid border-[1px] border-secondary`} onClick={()=>setIsOpen(!isOpen)}>
-        <p className='text-[1.25rem]'>{question}</p>
+        <p className='text-[.875rem] md:text-[1.25rem]'>{question}</p>
         <HiOutlineChevronDown className='text-secondary text-[1.5rem]' />
       </button>
       <div className={`${isOpen? 'h-max visible rounded-br-[8px] rounded-bl-[8px] py-[1.5rem]' : 'h-0 invisible'} flex w-full px-[1rem] bg-secondary-dark border-solid border-[1px] border-secondary`}>
-        <p className={`${isOpen? 'opacity-1 select-auto' : 'opacity-0 select-none'} text-[1.25rem] text-secondary font-inter`}>{answer}</p>
+        <p className={`${isOpen? 'opacity-1 select-auto' : 'opacity-0 select-none'} text-[.875rem] md:text-[1.25rem] text-secondary font-inter`}>{answer}</p>
       </div>
     </div>
   )
