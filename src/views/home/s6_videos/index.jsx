9d0b5ff6ff72a7d09f3eaac4921b920c75c6f8c9
chochoprogrammer <michochoprogrammer@gmail.com>
2024-01-03 00:07:17 +0100

    updated with twerked styling and done video responsiveness

--- src/views/home/s6_videos/index.jsx
+++ src/views/home/s6_videos/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Video = () => {
   return (
-    <div className='w-full flex flex-col gap-[3rem] lg:pt-[5rem] pb-[10rem] '>
+    <div className='w-full flex flex-col gap-[3rem] pb-[5rem] '>
       <h1 className="text-[1.25rem] text-[#ffffff] font-semibold font-montserrat tracking-[15%] w-full text-center ">VIDE0</h1>
       <p className="text-[1.25rem] mx-auto">Launching of the StarShip</p>
       <iframe
@@ -10,6 +10,7 @@ const Video = () => {
         title='StarSHIP Launch'
         allow='accelerometer; gyroscope;'
         allowFullScreen
+        className='mx-auto max-w-[40rem] w-[90%] h-screen max-h-[25rem]'
       ></iframe>
     </div>
   )
