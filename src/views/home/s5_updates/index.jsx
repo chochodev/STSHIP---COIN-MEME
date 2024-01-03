6c12e38e0cc133eaad8a37f2d8e151a97026b87f
chochoprogrammer <michochoprogrammer@gmail.com>
2024-01-03 08:18:14 +0100

    updated with react-twitter-embed instead of react-tweet to check if it was the source of the error

--- src/views/home/s5_updates/index.jsx
+++ src/views/home/s5_updates/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Tweet } from 'react-tweet'
+// import { Tweet } from 'react-tweet'
+import { TwitterTweetEmbed } from 'react-twitter-embed';
 
 const Update = () => {
  
@@ -7,9 +8,16 @@ const Update = () => {
     <div id='updates' className='w-full flex flex-col gap-[3rem] lg:pt-[5rem] pb-[10rem] '>
       <h1 className="text-[1.25rem] text-[#ffffff] font-semibold font-montserrat tracking-[15%] w-full text-center ">RECENT UPDATES</h1>
       <div className="flex flex-col items-center gap-[5rem] lg:gap-[8rem] ">
-        <Tweet tweetId={'1738276406537187814'} />
+        <TwitterTweetEmbed
+          tweetId={'1738276406537187814'}
+          options={{
+            theme: 'dark',
+            conversation: 'none',
+          }}
+        />
+        {/* <Tweet tweetId={'1738276406537187814'} />
         <Tweet tweetId={'1738258931804152307'} />
-        <Tweet tweetId={'1738248502864396695'} />
+        <Tweet tweetId={'1738248502864396695'} /> */}
       </div>
       
     </div>
