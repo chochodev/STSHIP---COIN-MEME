abfe2bcdb500dde85da39ef354aa3ec1c54059a3
chochoprogrammer <michochoprogrammer@gmail.com>
2024-01-03 09:24:07 +0100

    the build error was from unused variable

--- src/views/home/s5_updates/index.jsx
+++ src/views/home/s5_updates/index.jsx
@@ -7,9 +7,9 @@ const Update = () => {
     <div id='updates' className='w-full flex flex-col gap-[3rem] lg:pt-[5rem] pb-[10rem] '>
       <h1 className="text-[1.25rem] text-[#ffffff] font-semibold font-montserrat tracking-[15%] w-full text-center ">RECENT UPDATES</h1>
       <div className="flex flex-col items-center gap-[5rem] lg:gap-[8rem] ">
-        <Tweet tweetId={'1738276406537187814'} />
-        <Tweet tweetId={'1738258931804152307'} />
-        <Tweet tweetId={'1738248502864396695'} />
+        <Tweet id={'1738276406537187814'} />
+        <Tweet id={'1738258931804152307'} />
+        <Tweet id={'1738248502864396695'} />
       </div>
       
     </div>
