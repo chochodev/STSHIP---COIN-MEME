5c6a76314e0c3f1bd954965a655de14d8bb12196
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-01-06 10:22:31 +0100

    updating again

--- src/views/home/s2_howtobuy/index.jsx
+++ src/views/home/s2_howtobuy/index.jsx
@@ -27,19 +27,7 @@ const HowToBuy = () => {
   ]
   return (
     <div id='howtobuy' className='w-full flex flex-col pb-[8rem] gap-[3rem] '>
-      <h1 className="text-[1.25rem] text-[#ffffff] font-semibold font-montserrat tracking-[15%] w-full text-center ">HOW TO BUY</h1>
-      <div className="flex flex-col lg:flex-row justify-center lg:justify-evenly gap-y-[4rem] items-center w-full">
-        {items.map((item, index) => (
-          <div className="flex flex-col gap-[1rem] w-[80%] max-w-[25rem] lg:w-[22%] min-w-[18rem]" key={index}>
-            <div className="flex flex-col items-center bg-secondary-dark rounded-[8px] border-solid border-secondary-dim border-[1px] ">
-              <img src={item.image} alt={item.name} className={`${item.image_style} h-[18.75rem] w-full object-cover rounded-tl-[8px] rounded-tr-[8px]`} />
-              <p className="text-[1.25rem] text-center font-semibold font-inter text-secondary-dark bg-[#BEF4C3] w-max px-[1.5rem] py-[.875rem] rounded-[8px] transform translate-y-[-1rem]">{item.text}</p>
-            </div>
-            <a href={item.link} target="_blank" rel="noopener noreferrer" className='text-secondary text-[1.15rem] md:text-[1.5625rem] font-montserrat font-semibold text-center py-[.875rem] bg-secondary-dark hover:text-secondary-light hover:border-secondary-light active:border-[#ffffff] rounded-[8px] border-solid border-secondary-dim border-[1px] transition-all duration-200 ease-in-out'>BUY NOW</a>
-          </div>
-        ))}
-      </div>
-        
+      <>
       <h1 className="text-[1.25rem] text-[#ffffff] font-semibold font-montserrat tracking-[15%] w-full text-center ">SOLANA TOKEN</h1>
       <div className='flex flex-col gap-[1rem] p-[.5rem] sm:p-[1rem] w-full md:w-[90%] mx-auto bg-background rounded-[8px] border-solid border-[1px] border-secondary'>
         <div className='flex justify-between items-center gap-[1rem] md:gap-[3.5rem] '>
@@ -64,6 +52,35 @@ const HowToBuy = () => {
           <CopyToClipboardButton text='GHtLF7drbYXGTHX73uSxqPKkJUzDqcBNe2M9fzjJzr3j' />
         </div>
       </div>
+      </>
+
+      <>
+      <h1 className="text-[1.25rem] text-[#ffffff] font-semibold font-montserrat tracking-[15%] w-full text-center ">HOW TO BUY</h1>
+      <div className="flex flex-col lg:flex-row justify-center lg:justify-evenly gap-y-[4rem] items-center w-full">
+        {items.map((item, index) => (
+          <div className="flex flex-col gap-[1rem] w-[80%] max-w-[25rem] lg:w-[22%] min-w-[18rem]" key={index}>
+            <div className="flex flex-col items-center bg-secondary-dark rounded-[8px] border-solid border-secondary-dim border-[1px] ">
+              <img src={item.image} alt={item.name} className={`${item.image_style} h-[18.75rem] w-full object-cover rounded-tl-[8px] rounded-tr-[8px]`} />
+              <p className="text-[1.25rem] text-center font-semibold font-inter text-secondary-dark bg-[#BEF4C3] w-max px-[1.5rem] py-[.875rem] rounded-[8px] transform translate-y-[-1rem]">{item.text}</p>
+            </div>
+            <a href={item.link} target="_blank" rel="noopener noreferrer" className='text-secondary text-[1.15rem] md:text-[1.5625rem] font-montserrat font-semibold text-center py-[.875rem] bg-secondary-dark hover:text-secondary-light hover:border-secondary-light active:border-[#ffffff] rounded-[8px] border-solid border-secondary-dim border-[1px] transition-all duration-200 ease-in-out'>BUY NOW</a>
+          </div>
+        ))}
+      </div>
+      </>
+      
+      <>
+      <h1 className="text-[1.25rem] text-[#ffffff] font-semibold font-montserrat tracking-[15%] iw-full text-center ">ABOUT US</h1>
+      <div className='flex flex-col justify-center lg:justify-evenly gap-y-[2rem] items-center text-center w-full max-w-[45rem] mx-auto'>
+        <h2 className='text-secondary text-[1.15rem] md:text-[1.25rem] lg:text-[1.5rem] text-center font-semibold'>STARSHIP🚀 - $STSHIP MEME TOKEN</h2>
+        <p className='text-[1.05rem] md:text-[1.15rem] lg:text-[1.35rem]'>
+          $STSHIP is a meme-centric cryptocurrency token inspired by Elon Musk's SpaceX Starship rocket development. This unique Solana meme coin is community-driven and combines the excitement of space exploration with the popular appeal of memes.
+          <br/><br/>
+          The token is designed to capture the viral nature of its theme and is part of the broader narrative of successful meme tokens.
+        </p>
+      </div>
+      </>
+
     </div>
   );
 };
