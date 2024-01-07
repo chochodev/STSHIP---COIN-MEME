f6d6a143db7a67de5f6564166069f0f6c179af87
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-01-07 07:57:48 +0100

    updated with the custom twitter timeline embed responsive somewhat

--- src/components/twitter_container/index.jsx
+++ src/components/twitter_container/index.jsx
@@ -1,3 +1,32 @@
+// import React, { useEffect } from "react";
+
+// const TwitterContainer = () => {
+//   useEffect(() => {
+//     const script = document.createElement("script");
+//     script.src = "https://platform.twitter.com/widgets.js";
+//     document.getElementsByClassName("twitter-embed")[0].appendChild(script);
+//   }, []);
+
+//   return (
+//     <section className="twitterContainer">
+//       <div className="twitter-embed">
+//         <a
+//           className="twitter-timeline"
+//           data-theme="dark"
+//           data-tweet-limit="3"
+//           data-width="300"
+//           data-chrome="noheader nofooter noborders"
+//           href="https://twitter.com/StarShip_Sol"
+//         >
+//           Tweets by StarShip_Sol
+//         </a>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default TwitterContainer;
+
 import React, { useEffect } from "react";
 
 const TwitterContainer = () => {
@@ -10,29 +39,33 @@ const TwitterContainer = () => {
     anchor.setAttribute("class", "twitter-timeline");
     anchor.setAttribute("data-theme", "dark");
     anchor.setAttribute("data-tweet-limit", "3");
+    anchor.setAttribute("data-width", "100%"); // Set to 100% for responsiveness
     anchor.setAttribute("data-chrome", "noheader nofooter noborders");
-    anchor.setAttribute("href", "https://twitter.com/StarShip_Sol?ref_src=twsrc%5Etfw");
+    anchor.setAttribute("href", "https://twitter.com/StarShip_Sol");
     document.getElementsByClassName("twitter-embed")[0].appendChild(anchor);
 
     const script = document.createElement("script");
     script.setAttribute("src", "https://platform.twitter.com/widgets.js");
     document.getElementsByClassName("twitter-embed")[0].appendChild(script);
-
-    script.onload = () => {
-      const tweetContainer = document.querySelector(".twitter-embed");
-      const tweets = tweetContainer.querySelectorAll(".timeline-Tweet");
-      const tweetLimit = 3;
-
-      if (tweets.length > tweetLimit) {
-        tweets.forEach((tweet, index) => {
-          if (index >= tweetLimit) {
-            tweet.style.display = "none";
-          }
-        });
+  
+    const updateTwitterWidth = () => {
+      const twitterTimeline = document.querySelector(".twitter-embed a.twitter-timeline");
+      if (twitterTimeline) {
+        const width = window.innerWidth >= 768 ? 700 : 310;
+        twitterTimeline.setAttribute("data-width", `${width}`);
+        window.twttr.widgets.load();
       }
     };
+  
+    updateTwitterWidth();
+
+    window.addEventListener("resize", updateTwitterWidth);
+  
+    return () => {
+      window.removeEventListener("resize", updateTwitterWidth);
+    };
   }, []);
- // <a class="twitter-timeline" href="https://twitter.com/StarShip_Sol?ref_src=twsrc%5Etfw">Tweets by StarShip_Sol</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
   return (
     <section className="twitterContainer">
       <div className="twitter-embed"></div>
--- src/views/home/s5_updates/index.jsx
+++ src/views/home/s5_updates/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
-// import TwitterContainer from 'components/twitter_container';
+// import { TwitterTimelineEmbed } from 'react-twitter-embed';
+import TwitterContainer from 'components/twitter_container';
 
 const Update = () => {
  
@@ -8,11 +8,12 @@ const Update = () => {
     <div id='updates' className='w-full flex flex-col gap-[3rem] items-center lg:pt-[5rem] pb-[10rem] '>
       <h1 className="text-[1.25rem] text-[#ffffff] font-semibold font-montserrat tracking-[15%] w-full text-center ">RECENT UPDATES</h1>
       <div className="max-w-[55rem] ">
-      <TwitterTimelineEmbed
+      {/* <TwitterTimelineEmbed
         sourceType="profile"
         screenName="StarShip_Sol"
         options={{tweetLimit: '3'}}
-      />
+      /> */}
+      <TwitterContainer />
       </div>
     </div>
   );
